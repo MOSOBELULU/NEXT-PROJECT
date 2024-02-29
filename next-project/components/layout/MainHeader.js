c8f351abaae9876a1d182079a9378c31c9c4bc2c
Lulu Mosobe <125878495+MOSOBELULU@users.noreply.github.com>
2024-02-29 21:35:02 +0200

    WIP

--- next-project/components/layout/MainHeader.js
+++ next-project/components/layout/MainHeader.js
@@ -6,7 +6,7 @@ function MainHeader() {
   return (
     <header className={classes.header}>
       <div className={classes.logo}>
-        <Link href='/'>NextEvents</Link>
+        <Link href='/'>Check out events</Link>
       </div>
       <nav className={classes.navigation}>
         <ul>
